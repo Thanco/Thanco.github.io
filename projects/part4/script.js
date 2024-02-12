6d3b20c3dc4fd4c4eb84292e7c302c6db6639987
Thanco <th@nco.host>
2024-02-12 00:59:24 -0500

    Finish Project part 4

--- projects/part4/script.js
+++ projects/part4/script.js
@@ -3,3 +3,13 @@ const button = document.getElementById('main-nav-hamburger');
 button.addEventListener('click', function() {
     document.getElementById('main-nav-links').classList.toggle('hide-nav-links');
 });
+
+const currentPage = document.getElementById('current-page');
+switch (currentPage.innerHTML) {
+    case 'Menu':
+        const btnCart = document.getElementById('btn-cart');
+        btnCart.addEventListener('click', function() {
+            window.location.href = 'cart.html';
+        });
+        break;
+}
