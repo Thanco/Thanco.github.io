e5c07c31b27dee03627ef8625a49a66e06fde75d
Thanco <th@nco.host>
2024-02-12 00:00:44 -0500

    Start to part 4

--- projects/part3/script.js
+++
@@ -1,5 +0,0 @@
-// Hamburger menu
-const button = document.getElementById('main-nav-hamburger');
-button.addEventListener('click', function() {
-    document.getElementById('main-nav-links').classList.toggle('hide-nav-links');
-});
